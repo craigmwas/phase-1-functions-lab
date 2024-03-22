2de47606d0db052b14109c787c8b9cb3ac1b9f3a
Craig <craig.mwangi@student.moringaschool.com>
2024-03-22 17:26:53 +0300

    Labs

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,37 @@
 require ( './helpers.js' );
+function distanceFromHqInBlocks(location){
+  const hq = 42;
+  return Math.abs(hq - location);
+}
+function distanceFromHqInFeet(location){
+  const blocks = distanceFromHqInBlocks(location);
+  const feetperblock = 264;
+  return blocks * feetperblock;
+}
+function distanceTravelledInFeet(start, destination){
+  const distance = Math.abs(start - destination);
+  return distance * 264;
+}
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+if (distance <= 400) {
+  return 0;
+}
+else if (distance >= 400 && distance < 2000) {
+  return (distance - 400) * 0.02;
+}
+else if (distance >= 2000 && distance < 2500) {
+  return 25;
+}
+else {
+  return 'cannot travel that far';
+
+
+}
+}
+
+
+
 
 describe('index.js', function() {
   describe('distanceFromHqInBlocks()', function() {
